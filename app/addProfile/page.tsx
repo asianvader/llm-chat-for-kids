8a9344d6bc1dc7296ee32cbdfcd73c29dcc9cbaa
Phoebe Voong-Fadel <phoebe.voong@gmail.com>
2023-11-14 11:23:55 +0000

    style: add profile form.

--- app/addProfile/page.tsx
+++ app/addProfile/page.tsx
@@ -46,30 +46,56 @@ const AddProfile = () => {
     }
   };
   return (
-    <div>
-      <h1>Create a profile</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">First name</label>
+    <div className="w-full max-w-xs">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-xl font-bold mb-6 text-center">Create a profile</h2>
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="name"
+        >
+          First name
+        </label>
         <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="name"
           id="name"
           required
           value={name}
           onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
         />
-        {errors.name && <p>{errors.name}</p>}
-        <label htmlFor="age">Age</label>
+        {errors.name && (
+          <p className="text-red-500 text-xs italic">{errors.name}</p>
+        )}
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="age"
+        >
+          Age
+        </label>
         <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="age"
           id="age"
           required
           value={age}
           onChange={(e) => setAge(e.target.value)}
+          placeholder="Age"
         />
-        {errors.age && <p>{errors.age}</p>}
-        <button type="submit">Submit</button>
+        {errors.age && (
+          <p className="text-red-500 text-xs italic">{errors.age}</p>
+        )}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-6"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
