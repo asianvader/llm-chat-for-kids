912dc294169624583cca128397c67febc5d3e877
Phoebe Voong-Fadel <phoebe.voong@gmail.com>
2023-11-30 10:31:19 +0000

    feat: add back navigation button

--- app/accountSettings/page.tsx
+++ app/accountSettings/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import { EditProfile } from "@/components/EditProfile";
 import Logout from "@/components/Logout";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 function accountSettings() {
+  const router = useRouter();
   return (
     <div className="bg-yellow-200 min-h-screen flex flex-col">
-      <header className="bg-blue-800 text-white pt-4 pl-5 pb-1 sticky top-0 z-10 flex justify-between shadow-md">
+      <header className="bg-blue-800 text-white pt-4 pl-5 pb-2 sticky top-0 z-10 flex justify-between shadow-md">
+        <button aria-label="Navigate back" onClick={() => router.back()}>
+          <ArrowLeftIcon className="h-8 w-8 text-white-800 hover:text-gray-400" />
+        </button>
+
         <h2 className="text-2xl font-semibold">Account Settings</h2>
         <Logout />
       </header>
